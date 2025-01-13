5fba72f908027325424f2ac397fc2880e21454ce
lukvmil <millerluke1364@gmail.com>
2025-01-13 11:22:18 +0800

    automatic background syncing with KOI API, manual refresh command, settings to configure remote endpoint, API key, etc. mutex for non conflicting interaction with KOI directory

--- main.ts
+++ main.ts
@@ -1,85 +1,238 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, normalizePath, Plugin, PluginSettingTab, requestUrl, Setting } from 'obsidian';
+import * as Handlebars from 'handlebars';
+import { Mutex } from 'async-mutex';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+interface KoiPluginSettings {
+	koiApiUrl: string;
+	koiApiKey: string;
+	koiApiSubscriberId: string;
+	koiSyncDirectoryPath: string;
+	templatePath: string;
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: KoiPluginSettings = {
+	koiApiUrl: "https://telescope-koi.lukvmil.com",
+	koiApiKey: "",
+	koiApiSubscriberId: "",
+	koiSyncDirectoryPath: "telescope",
+	templatePath: ""
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class KoiPlugin extends Plugin {
+	settings: KoiPluginSettings;
+	statusBar: HTMLElement;
+	connected: boolean = true;
+	mutex = new Mutex();
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+		console.log(this.settings.koiApiUrl, this.settings.koiApiKey);
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+		this.syncKoi();
+		this.registerInterval(
+			window.setInterval(() => this.syncKoi(), 5 * 1000)
+		);
+		
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new KoiSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+		this.statusBar = this.addStatusBarItem();
+		await this.updateStatusBar();
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
+		this.addCommand({
+			id: 'refresh-with-koi',
+			name: 'Refresh with KOI',
+			callback: () => {
+				this.refreshKoi();
+			}
+		})
+
+		this.addCommand({
+			id: 'sync-with-koi',
+			name: 'Sync with KOI',
+			callback: () => {
+				this.syncKoi();
+			}
+		})
 	}
 
 	onunload() {
 
+	}
+
+	async readLocalRids() {
+		const rids = [];
+		let listedfiles = await this.app.vault.adapter.list(this.settings.koiSyncDirectoryPath);
+		for (const file_path of listedfiles.files) {
+			const file_name = file_path.slice(
+				this.settings.koiSyncDirectoryPath.length + 1,
+				-3
+			)
+			const rid = atob(file_name);
+			rids.push(rid);
+		}
+
+		return rids;
+	}
+
+	getRidPath(rid: string) {
+		return normalizePath(`${this.settings.koiSyncDirectoryPath}/${btoa(rid)}.md`);
+	}
+
+	getRidFile(rid: string) {
+		return this.app.vault.getAbstractFileByPath(this.getRidPath(rid));
+	}
+
+	async updateStatusBar() {
+		let emoji = "✅"
+		if (this.mutex.isLocked())
+			emoji = "🔄";
+
+		if (!this.connected)
+			emoji = "❌";
+
+		this.statusBar.setText(
+			`KOI - ${emoji}`
+		)
+	}
+
+	async callKoiApi(path: string, method: string = "GET") {
+		try {
+			const resp = await requestUrl({
+				url: this.settings.koiApiUrl + path,
+				headers: {
+					"X-API-Key": this.settings.koiApiKey
+				},
+				method: method,
+				throw: false
+			})
+			this.connected = true;
+			let data = resp.json;
+			return resp;
+		} catch (e) {
+			this.connected = false;
+			throw e;
+		} finally {
+			this.updateStatusBar();
+		}
+	}
+
+	async retrieveRidObject(rid: string) {
+		let response = await this.callKoiApi(`/object?rid=${rid}`)
+
+		let bundle = response.json;
+
+		let msg_text = bundle.contents.text;
+		delete bundle.contents.text;
+
+		let text: string = "---\n" + JSON.stringify(bundle.contents) + "\n---\n>" + msg_text;
+
+		this.app.vault.adapter.write(
+			this.getRidPath(rid),
+			text
+		)
+	}
+
+	async registerAsSubscriber() {
+		const resp = await this.callKoiApi("/events/subscribe", "POST");
+		this.settings.koiApiSubscriberId = resp.json.subscriber_id;
+		await this.saveSettings();
+	}
+
+	async syncKoi() {
+		if (this.settings.koiApiSubscriberId == "")
+			this.registerAsSubscriber();
+
+		const resp = await this.callKoiApi(`/events/poll/${this.settings.koiApiSubscriberId}`);
+
+		if (resp.status == 404) {
+			this.registerAsSubscriber();
+			this.refreshKoi();
+			return;
+		}
+
+		let events = resp.json;
+		if (events.length == 0) return;
+
+		console.log("attempting sync");
+	
+
+		const release = await this.mutex.acquire();
+
+		try {
+
+			console.log("started sync");
+			this.updateStatusBar();
+
+			console.log(`processing ${events.length} events`)
+
+			for (const event of events) {
+				console.log(`${event.event_type}: ${event.rid}`);
+
+				if (event.event_type == 'NEW' || event.event_type == 'UPDATE') {
+					await this.retrieveRidObject(event.rid);
+					
+				} else if (event.event_type == 'FORGET') {
+					let file = this.getRidFile(event.rid);
+					if (file != null)
+						this.app.vault.delete(file);
+				}
+			}
+
+			
+			console.log("ended sync");
+		
+		} finally {
+			release();
+
+			this.updateStatusBar();
+		}
+	}
+
+	async refreshKoi() {
+		console.log("attempting refresh");
+		const release = await this.mutex.acquire();
+
+		try {
+			this.updateStatusBar();
+
+			console.log("started refresh");
+
+			const response = await this.callKoiApi("/rids");
+
+			let folder = this.app.vault.getFolderByPath("koi");
+			if (folder == null) {
+				this.app.vault.createFolder("koi");
+				console.log("couldn't find folder, creating new one");
+			}
+
+			const rids: string[] = response.json;
+
+			console.log(`fetched ${rids.length} rids`);
+
+			for (const rid of (await this.readLocalRids())) {
+				if (!rids.includes(rid)) {
+					let file = this.getRidFile(rid);
+					if (file != null)
+						this.app.vault.delete(file);
+				}
+			}
+
+			for (const rid of rids) {
+				console.log(`retrieving ${rid}`);
+				await this.retrieveRidObject(rid);
+			}
+		} finally {
+
+			console.log("ended refresh");
+
+			release();
+			this.updateStatusBar();
+		}
 	}
 
 	async loadSettings() {
@@ -91,26 +244,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class KoiSettingTab extends PluginSettingTab {
+	plugin: KoiPlugin;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: KoiPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -121,13 +258,56 @@ class SampleSettingTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('KOI API URL')
+			.setDesc('URL of the KOI API this plugin will communicate with')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('https://...')
+				.setValue(this.plugin.settings.koiApiUrl)
 				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.koiApiUrl = value;
+					await this.plugin.saveSettings();
+				}));
+		
+		new Setting(containerEl)
+			.setName('KOI API Key')
+			.setDesc('Key for accessing KOI API')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.koiApiKey)
+				.onChange(async (value) => {
+					this.plugin.settings.koiApiKey = value;
+					await this.plugin.saveSettings();
+				}));
+
+		new Setting(containerEl)
+			.setName('KOI API Subscriber ID')
+			.setDesc('Subscriber ID for receiving RID events (set automatically)')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.koiApiSubscriberId
+			))
+			.setDisabled(true);
+
+		new Setting(containerEl)
+			.setName('KOI Sync Directory Path')
+			.setDesc('Directory used to store RID objects synced through KOI')
+			.addText(text => text
+				.setPlaceholder('koi')
+				.setValue(this.plugin.settings.koiSyncDirectoryPath)
+				.onChange(async (value) => {
+					this.plugin.settings.koiSyncDirectoryPath = value;
+					await this.plugin.saveSettings();
+				})
+			)
+		
+		new Setting(containerEl)
+			.setName('Template file location')
+			.setDesc('Will be used to generate documents imported from KOI')
+			.addText(text => text
+				.setPlaceholder('')
+				.setValue(this.plugin.settings.templatePath)
+				.onChange(async (value) => {
+					this.plugin.settings.templatePath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
